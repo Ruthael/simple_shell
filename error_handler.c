560ab0aea4a850bf41056eddba658ff7c639423b
Ruthael <110993814+Ruthael@users.noreply.github.com>
2022-11-16 18:46:06 +0300

    Update error_handler.c

--- error_handler.c
+++ error_handler.c
@@ -1,86 +1,110 @@
-#include "shell.h"
+#include "main.h"
 
 int num_len(int num);
 char *_itoa(int num);
-int create_error(char *name, int hist, char *command, int err);
+int create_error(char **args, int err);
 
 /**
- * num_len - Counts the digit length of a number.
- * @num: The number to measure.
- *
- * Return: The digit length.
- */
+* num_len - Counts the digit length of a number.
+* @num: The number to measure.
+* Return: The digit length.
+*/
 int num_len(int num)
 {
+	unsigned int num1;
 	int len = 1;
-	while (num > 9)
+
+	if (num < 0)
 	{
 		len++;
-		num /= 10;
+		num1 = num * -1;
 	}
+	else
+	{
+		num1 = num;
+	}
+	while (num1 > 9)
+	{
+		len++;
+		num1 /= 10;
+	}
+
 	return (len);
 }
 
 /**
- * _itoa - Converts an integer to a string.
- * @num: The integer.
- *
- * Return: The converted string.
- */
+* _itoa - Converts an integer to a string.
+* @num: The integer.
+* Return: The converted string.
+*/
 char *_itoa(int num)
 {
 	char *buffer;
 	int len = num_len(num);
+	unsigned int num1;
+
 	buffer = malloc(sizeof(char) * (len + 1));
 	if (!buffer)
 		return (NULL);
+
 	buffer[len] = '\0';
-	len--;
-	while (len >= 0)
+
+	if (num < 0)
 	{
-		buffer[len] = (num % 10) + '0';
-		num /= 10;
-		len--;
+		num1 = num * -1;
+		buffer[0] = '-';
 	}
+	else
+	{
+		num1 = num;
+	}
+
+	len--;
+	do {
+		buffer[len] = (num1 % 10) + '0';
+		num1 /= 10;
+		len--;
+	} while (num1 > 0);
+
 	return (buffer);
 }
 
-
 /**
- * create_error - Writes a custom error message to stderr.
- * @name: The name of the call causing the error.
- * @hist: The history number of the call.
- * @command: The command causing the error.
- * @err: The error value.
- *
- * Return: The error value.
- */
-int create_error(char *name, int hist, char *command, int err)
+* create_error - Writes a custom error message to stderr.
+* @args: An array of arguments.
+* @err: The error value.
+* Return: The error value.
+*/
+int create_error(char **args, int err)
 {
-	char *error, *hist_str;
-	int len;
-	hist_str = _itoa(hist);
-	if (!hist_str)
-		/* TODO return a better error number for number conversion fail */
-		return (err);
-	len = strlen(name) + strlen(hist_str) + strlen(command) + 6;
-	if (err == 127)
-		len += 10;
-	else
-		len += 18;
-	error = malloc(sizeof(char) * (len + 1));
-	if (!error)
-		/* TODO return a better error number for malloc failure */
-		return (err);
-	strcpy(error, name);
-	strcat(error, ": ");
-	strcat(error, hist_str);
-	strcat(error, ": ");
-	strcat(error, command);
-	if (err == 127)
-		strcat(error, ": not found\n");
-	else
-		strcat(error, ": Permission denied\n");
-	write(STDERR_FILENO, error, len);
+	char *error;
+
+	switch (err)
+	{
+	case -1:
+		error = error_env(args);
+		break;
+	case 1:
+		error = error_1(args);
+		break;
+	case 2:
+		if (*(args[0]) == 'e')
+			error = error_2_exit(++args);
+		else if (args[0][0] == ';' || args[0][0] == '&' || args[0][0] == '|')
+			error = error_2_syntax(args);
+		else
+			error = error_2_cd(args);
+		break;
+	case 126:
+		error = error_126(args);
+		break;
+	case 127:
+		error = error_127(args);
+		break;
+	}
+	write(STDERR_FILENO, error, _strlen(error));
+
+	if (error)
+		free(error);
 	return (err);
 }
