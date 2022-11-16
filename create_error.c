d3e760090e9eafb600f099abebd3f73bf48ab2bd
berhanez <birhanez@gmail.com>
2022-11-16 13:00:18 +0300

    helper functions added, error handler and error message printer

---
+++ create_error.c
@@ -0,0 +1,137 @@
+#include "main.h"
+char *error_2(char *name, int hist, char **args);
+char *error_126(char *name, int hist, char **args);
+char *error_127(char *name, int hist, char **args);
+/**
+ * error_2 - Creates an error message for shellby_exit errors.
+ * @name: The command name.
+ * @hist: The history number of the command.
+ * @args: An array of arguments passed to the command.
+ *
+ * Return: The error string.
+ */
+char *error_2(char *name, int hist, char **args)
+{
+	char *error, *hist_str;
+	int len;
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (NULL);
+	len = strlen(name) + strlen(hist_str) + strlen(args[0])
+	      + strlen(args[1]) + 23;
+	error = malloc(sizeof(char) * (len + 1));
+	if (!error)
+	{
+		free(hist_str);
+		return (NULL);
+	}
+	strcpy(error, name);
+	strcat(error, ": ");
+	strcat(error, hist_str);
+	strcat(error, ": ");
+	strcat(error, args[0]);
+	strcat(error, ": ");
+	strcat(error, "Illegal Number");
+	strcat(error, ": ");
+	strcat(error, args[1]);
+	strcat(error, "\n");
+	free(hist_str);
+	return (error);
+}
+
+char *error_2cd(char *name, int hist, char **args)
+{
+	char *error, *hist_str;
+	int len;
+
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (NULL);
+
+	len = strlen(name) + strlen(hist_str) + strlen(args[0])
+	      + strlen(args[1]) + 18;
+	error = malloc(sizeof(char) * (len + 1));
+	if (!error)
+	{
+		free(hist_str);
+		return (NULL);
+	}
+
+	strcpy(error, name);
+	strcat(error, ": ");
+	strcat(error, hist_str);
+	strcat(error, ": ");
+	strcat(error, args[0]);
+	strcat(error, ": ");
+	strcat(error, "can't cd to ");
+	strcat(error, args[1]);
+	strcat(error, "\n");
+
+	free(hist_str);
+	return (error);
+}
+
+/**
+ * error_126 - Creates an error message for permission denied failures.
+ * @name: The command name.
+ * @hist: The history number of the command.
+ * @args: An array of arguments passed to the command.
+ *
+ * Return: The error string.
+ */
+char *error_126(char *name, int hist, char **args)
+{
+	char *error, *hist_str;
+	int len;
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (NULL);
+	len = strlen(name) + strlen(hist_str) + strlen(args[0]) + 24;
+	error = malloc(sizeof(char) * (len + 1));
+	if (!error)
+	{
+		free(hist_str);
+		return (NULL);
+	}
+	strcpy(error, name);
+	strcat(error, ": ");
+	strcat(error, hist_str);
+	strcat(error, ": ");
+	strcat(error, args[0]);
+	strcat(error, ": ");
+	strcat(error, "Permission denied\n");
+	free(hist_str);
+	return (error);
+}
+/**
+ * error_127 - Creates an error message for command not found failures.
+ * @name: The command name.
+ * @hist: The history number of the command.
+ * @args: An array of arguments passed to the command.
+ *
+ * Return: The error string.
+ */
+char *error_127(char *name, int hist, char **args)
+{
+	char *error, *hist_str;
+	int len;
+	hist_str = _itoa(hist);
+	if (!hist_str)
+		return (NULL);
+	len = strlen(name) + strlen(hist_str) + strlen(args[0]) + 16;
+	error = malloc(sizeof(char) * (len + 1));
+	if (!error)
+	{
+		free(hist_str);
+		return (NULL);
+	}
+	strcpy(error, name);
+	strcat(error, ": ");
+	strcat(error, hist_str);
+	strcat(error, ": ");
+	strcat(error, args[0]);
+	strcat(error, ": ");
+	strcat(error, "not found\n");
+	free(hist_str);
+	return (error);
+}
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
@@ -10,7 +11,7 @@
 #include <unistd.h>
 
 extern char **environ;
-t/**
+/**
  * struct list_s - A new struct type defining a linked list.
  * @dir: A directory path.
  * @next: A pointer to another struct list_s.
@@ -20,17 +21,37 @@ typedef struct list_s
 	char *dir;
 	struct list_s *next;
 } list_t;
-
+/**
+ * struct builtin_s - A structure type defining all builtin commands.
+ * @name: The name of the builtin command.
+ * @f: A function pointer to the command's function.
+ */
+typedef struct builtin_s
+{
+	char *name;
+	int (*f)(char **argv);
+} builtin_t;
+/* Helper Functions */
 char **_strtok(char *line, char *delim);
 char *get_location(char *command);
 list_t *get_path_dir(char *path);
 list_t *add_node_end(list_t **head, char *dir);
 void free_list(list_t *head);
-
+int num_len(int num);
+char *_itoa(int num);
+int create_error(char *name, int hist, char **argv, int err);
 /* Builtins */
+int (*get_builtin(char *command))(char **argv);
+int shellby_exit(char **argv);
 char *_getenv(const char *name);
 int _setenv(const char *name, const char *value, int overwrite);
 int _unsetenv(const char *name);
-
+/* Error handling */
+int num_len(int num);
+char *_itoa(int num);
+int create_error(char *name, int hist, char **argv, int err);
+char *error_2(char *name, int hist, char **argv);
+char *error_126(char *name, int hist, char **argv);
+char *error_127(char *name, int hist, char **argv);
 
 #endif /* _MAIN_H_ */
