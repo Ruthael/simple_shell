7c0d6700a2b4c04ed5b1480d9770ae257f1e03fd
Ruthael <110993814+Ruthael@users.noreply.github.com>
2022-11-16 18:33:08 +0300

    fixing bltin_help_fun1

--- bltin_help_fun1.c
+++ bltin_help_fun1.c
@@ -6,8 +6,8 @@ void help_unsetenv(void);
 void help_history(void);
 
 /**
- * help_env - Displays information on the shellby builtin command 'env'.
- */
+* help_env - show info on the simple_shell builtin command 'env'.
+*/
 void help_env(void)
 {
 	char *msg = "env: env\n\tPrints the current environment.\n";
@@ -16,8 +16,8 @@ void help_env(void)
 }
 
 /**
- * help_setenv - Displays information on the shellby builtin command 'setenv'.
- */
+* help_setenv - show info on the simple_shell builtin command 'setenv'.
+*/
 void help_setenv(void)
 {
 	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
@@ -26,5 +26,20 @@ void help_setenv(void)
 	msg = "environment variable, or modifies an existing one.\n\n";
 	write(STDOUT_FILENO, msg, _strlen(msg));
 	msg = "\tUpon failure, prints a message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+* help_unsetenv - show info on the simple_shell builtin command
+* 'unsetenv'.
+*/
+void help_unsetenv(void)
+{
+	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environmental variable.\n\n\tUpon failure, prints a ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "message to stderr.\n";
 	write(STDOUT_FILENO, msg, _strlen(msg));
 }
