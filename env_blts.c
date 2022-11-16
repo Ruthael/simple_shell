69dbc942a609a74b5016669371daac27a260686c
Ruthael <110993814+Ruthael@users.noreply.github.com>
2022-11-16 18:43:08 +0300

    debugging

--- env_blts.c
+++ env_blts.c
@@ -1,57 +1,57 @@
-#include "shell.h"
-char **_get_env(const char *name);
-int shellby_setenv(char **args);
-int shellby_unsetenv(char **args);
-char **_get_env(const char *name);
+#include "main.h"
+
+int shell_env(char **args, char __attribute__((__unused__)) **front);
+int shell_setenv(char **args, char __attribute__((__unused__)) **front);
+int shell_unsetenv(char **args, char __attribute__((__unused__)) **front);
 /**
- * shellby_env - Prints the current environment.
- * @args: An array of arguments passed to the shell.
- *
- * Return: If an error occurs - -1.
- *	   Otherwise - 0.
- *
- * Description: Prints one variable per line in the
- *              format 'variable'='value'.
- */
-int shellby_env(char **args)
+* shell_env - prints current environment
+* @args: array of arguments passed to shell
+* @front: double pointer to the beginning of args
+* Return: 0 on success, -1 for error
+* Description: Prints one variable per line
+*/
+int shell_env(char **args, char __attribute__((__unused__)) **front)
 {
 	int index;
 	char nc = '\n';
+
 	if (!environ)
 		return (-1);
+
 	for (index = 0; environ[index]; index++)
 	{
-		write(STDOUT_FILENO, environ[index], strlen(environ[index]));
+		write(STDOUT_FILENO, environ[index], _strlen(environ[index]));
 		write(STDOUT_FILENO, &nc, 1);
 	}
+
 	(void)args;
 	return (0);
 }
+
 /**
- * shellby_setenv - Changes or adds an environmental variable to the PATH.
- * @args: An array of arguments passed to the shell.
- * Description: args[1] is the name of the new or existing PATH variable.
- *              args[2] is the value to set the new or changed variable to.
- *
- * Return: If an error occurs - -1.
- *         Otherwise - 0.
- */
-int shellby_setenv(char **args)
+* shell_setenv - changes or adds an env var to the PATH.
+* @args: array of arguments passed to the shell.
+* @front: A double pointer to the beginning of args.
+* Description: args[1] is the name of the new or existing PATH variable.
+* args[2] is the value to set the new or changed variable to.
+* Return: 0 on success, -1 for error
+*/
+int shell_setenv(char **args, char __attribute__((__unused__)) **front)
 {
 	char **env_var = NULL, **new_environ, *new_value;
 	size_t size;
 	int index;
 
 	if (!args[0] || !args[1])
-		return (-1);
+		return (create_error(args, -1));
 
-	new_value = malloc(strlen(args[0]) + 1 + strlen(args[1]) + 1);
+	new_value = malloc(_strlen(args[0]) + 1 + _strlen(args[1]) + 1);
 	if (!new_value)
-		return (-1);
+		return (create_error(args, -1));
+	_strcpy(new_value, args[0]);
+	_strcat(new_value, "=");
+	_strcat(new_value, args[1]);
 
-	strcpy(new_value, args[0]);
-	strcat(new_value, "=");
-	strcat(new_value, args[1]);
 	env_var = _getenv(args[0]);
 	if (env_var)
 	{
@@ -61,46 +61,52 @@ int shellby_setenv(char **args)
 	}
 	for (size = 0; environ[size]; size++)
 		;
+
 	new_environ = malloc(sizeof(char *) * (size + 2));
 	if (!new_environ)
 	{
 		free(new_value);
-		return (-1);
+		return (create_error(args, -1));
 	}
+
 	for (index = 0; environ[index]; index++)
 		new_environ[index] = environ[index];
+
 	free(environ);
 	environ = new_environ;
 	environ[index] = new_value;
 	environ[index + 1] = NULL;
+
 	return (0);
 }
+
 /**
- * shellby_unsetenv - Deletes an environmental variable from the PATH.
+ * shell_unsetenv - Deletes an environmental variable from the PATH.
  * @args: An array of arguments passed to the shell.
+ * @front: A double pointer to the beginning of args.
  * Description: args[1] is the PATH variable to remove.
  *
- * Return: If an error occurs - -1.
- *         Otherwise - 0.
+* Return: 0 on success, -1 for error
  */
-int shellby_unsetenv(char **args)
+int shell_unsetenv(char **args, char __attribute__((__unused__)) **front)
 {
 	char **env_var, **new_environ;
 	size_t size;
 	int index, index2;
 
 	if (!args[0])
-		return (-1);
-
+		return (create_error(args, -1));
 	env_var = _getenv(args[0]);
 	if (!env_var)
 		return (0);
 
 	for (size = 0; environ[size]; size++)
 		;
+
 	new_environ = malloc(sizeof(char *) * size);
 	if (!new_environ)
-		return (-1);
+		return (create_error(args, -1));
+
 	for (index = 0, index2 = 0; environ[index]; index++)
 	{
 		if (*env_var == environ[index])
