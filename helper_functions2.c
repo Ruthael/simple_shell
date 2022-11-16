9f9b69e38b1dff2ab7894cc53f7158277c62d425
Ruthael <110993814+Ruthael@users.noreply.github.com>
2022-11-16 18:51:05 +0300

    fixing helper_functions2

--- helper_functions2.c
+++ helper_functions2.c
@@ -2,15 +2,15 @@
 
 void handle_line(char **line, ssize_t read);
 ssize_t get_new_len(char *line);
+void logical_ops(char *line, ssize_t *new_len);
 
 /**
- * handle_line - Partitions a line read from standard input as needed.
- * @line: A pointer to a line read from standard input.
- * @read: The length of line.
- *
- * Description: Spaces are inserted to separate ";", "||", and "&&".
- *              Replaces "#" with '\0'.
- */
+* handle_line - Partitions a line read from standard input as needed.
+* @line: A pointer to a line read from standard input.
+* @read: The length of line.
+* Description: Spaces are inserted to separate ";", "||", and "&&".
+* Replaces "#" with '\0'.
+*/
 void handle_line(char **line, ssize_t read)
 {
 	char *old_line, *new_line;
@@ -95,15 +95,12 @@ void handle_line(char **line, ssize_t read)
 }
 
 /**
- * get_new_len - Gets the new length of a line partitioned
- *               by ";", "||", "&&&", or "#".
- * @line: The line to check.
- *
- * Return: The new length of the line.
- *
- * Description: Cuts short lines containing '#' comments with '\0'.
- */
-
+* get_new_len - Gets the new length of a line partitioned
+* by ";", "||", "&&&", or "#".
+* @line: The line to check.
+* Return: The new length of the line.
+* Description: Cuts short lines containing '#' comments with '\0'.
+*/
 ssize_t get_new_len(char *line)
 {
 	size_t i;
@@ -154,4 +151,33 @@ ssize_t get_new_len(char *line)
 		new_len++;
 	}
 	return (new_len);
+}
+
+/**
+* logical_ops - Checks a line for logical operators "||" or "&&".
+* @line: A pointer to the character to check in the line.
+* @new_len: Pointer to new_len in get_new_len function.
+*/
+void logical_ops(char *line, ssize_t *new_len)
+{
+	char previous, current, next;
+
+	previous = *(line - 1);
+	current = *line;
+	next = *(line + 1);
+
+	if (current == '&')
+	{
+		if (next == '&' && previous != ' ')
+			(*new_len)++;
+		else if (previous == '&' && next != ' ')
+			(*new_len)++;
+	}
+	else if (current == '|')
+	{
+		if (next == '|' && previous != ' ')
+			(*new_len)++;
+		else if (previous == '|' && next != ' ')
+			(*new_len)++;
+	}
 }
