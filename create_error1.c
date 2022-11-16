d38d952d73f260d11290caf8ad5b35043a04eb7b
Ruthael <110993814+Ruthael@users.noreply.github.com>
2022-11-16 18:37:38 +0300

    debugging

--- create_error1.c
+++ create_error1.c
@@ -1,45 +1,13 @@
 #include "main.h"
-char *error_2(char *name, int hist, char **args);
-char *error_126(char *name, int hist, char **args);
-char *error_127(char *name, int hist, char **args);
+
+char *error_126(char **args);
+char *error_127(char **args);
 /**
- * error_2 - Creates an error message for shellby_exit errors.
- * @name: The command name.
- * @hist: The history number of the command.
- * @args: An array of arguments passed to the command.
- *
- * Return: The error string.
- */
-char *error_2(char *name, int hist, char **args)
-{
-	char *error, *hist_str;
-	int len;
-	hist_str = _itoa(hist);
-	if (!hist_str)
-		return (NULL);
-	len = strlen(name) + strlen(hist_str) + strlen(args[0])
-	      + strlen(args[1]) + 23;
-	error = malloc(sizeof(char) * (len + 1));
-	if (!error)
-	{
-		free(hist_str);
-		return (NULL);
-	}
-	strcpy(error, name);
-	strcat(error, ": ");
-	strcat(error, hist_str);
-	strcat(error, ": ");
-	strcat(error, args[0]);
-	strcat(error, ": ");
-	strcat(error, "Illegal Number");
-	strcat(error, ": ");
-	strcat(error, args[1]);
-	strcat(error, "\n");
-	free(hist_str);
-	return (error);
-}
-
-char *error_2cd(char *name, int hist, char **args)
+* error_126 - error message for permission denied failures
+* @args: array of arguments passed to the command.
+* Return: The error string.
+*/
+char *error_126(char **args)
 {
 	char *error, *hist_str;
 	int len;
@@ -48,8 +16,7 @@ char *error_2cd(char *name, int hist, char **args)
 	if (!hist_str)
 		return (NULL);
 
-	len = strlen(name) + strlen(hist_str) + strlen(args[0])
-	      + strlen(args[1]) + 18;
+	len = _strlen(name) + _strlen(hist_str) + _strlen(args[0]) + 24;
 	error = malloc(sizeof(char) * (len + 1));
 	if (!error)
 	{
@@ -57,81 +24,46 @@ char *error_2cd(char *name, int hist, char **args)
 		return (NULL);
 	}
 
-	strcpy(error, name);
-	strcat(error, ": ");
-	strcat(error, hist_str);
-	strcat(error, ": ");
-	strcat(error, args[0]);
-	strcat(error, ": ");
-	strcat(error, "can't cd to ");
-	strcat(error, args[1]);
-	strcat(error, "\n");
+	_strcpy(error, name);
+	_strcat(error, ": ");
+	_strcat(error, hist_str);
+	_strcat(error, ": ");
+	_strcat(error, args[0]);
+	_strcat(error, ": Permission denied\n");
 
 	free(hist_str);
 	return (error);
 }
 
 /**
- * error_126 - Creates an error message for permission denied failures.
- * @name: The command name.
- * @hist: The history number of the command.
- * @args: An array of arguments passed to the command.
- *
- * Return: The error string.
- */
-char *error_126(char *name, int hist, char **args)
+* error_127 - error message for command not found failures.
+* @args: array of arguments passed to the command.
+* Return: The error string.
+*/
+char *error_127(char **args)
 {
 	char *error, *hist_str;
 	int len;
+
 	hist_str = _itoa(hist);
 	if (!hist_str)
 		return (NULL);
-	len = strlen(name) + strlen(hist_str) + strlen(args[0]) + 24;
+
+	len = _strlen(name) + _strlen(hist_str) + _strlen(args[0]) + 16;
 	error = malloc(sizeof(char) * (len + 1));
 	if (!error)
 	{
 		free(hist_str);
 		return (NULL);
 	}
-	strcpy(error, name);
-	strcat(error, ": ");
-	strcat(error, hist_str);
-	strcat(error, ": ");
-	strcat(error, args[0]);
-	strcat(error, ": ");
-	strcat(error, "Permission denied\n");
-	free(hist_str);
-	return (error);
-}
-/**
- * error_127 - Creates an error message for command not found failures.
- * @name: The command name.
- * @hist: The history number of the command.
- * @args: An array of arguments passed to the command.
- *
- * Return: The error string.
- */
-char *error_127(char *name, int hist, char **args)
-{
-	char *error, *hist_str;
-	int len;
-	hist_str = _itoa(hist);
-	if (!hist_str)
-		return (NULL);
-	len = strlen(name) + strlen(hist_str) + strlen(args[0]) + 16;
-	error = malloc(sizeof(char) * (len + 1));
-	if (!error)
-	{
-		free(hist_str);
-		return (NULL);
-	}
-	strcpy(error, name);
-	strcat(error, ": ");
-	strcat(error, hist_str);
-	strcat(error, ": ");
-	strcat(error, args[0]);
-	strcat(error, ": ");
-	strcat(error, "not found\n");
+
+	_strcpy(error, name);
+	_strcat(error, ": ");
+	_strcat(error, hist_str);
+	_strcat(error, ": ");
+	_strcat(error, args[0]);
+	_strcat(error, ": not found\n");
+
 	free(hist_str);
 	return (error);
 }
